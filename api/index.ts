abad38fb51af7a2eb3caf4546481648499c8fa84
communi <expocommuni@gmail.com>
2024-10-28 22:57:22 -0600

    ready

---
+++ api/index.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import { config } from "dotenv";
+import SQLDataSource from "../src/config/NeonDataSource";
+import MongoDataSource from "../src/config/MongoDataSource";
+import routes from "../src/routes/index";
+import "reflect-metadata";
+import 'tsconfig-paths/register';
+import cors from "cors";  // Importa cors
+config();
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json({limit: '50mb'})); 
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+
+// Initialize SQL Database
+SQLDataSource.initialize()
+  .then(() => {
+    console.log("Connection established with the SQL database");
+
+    // Initialize MongoDB if needed
+    MongoDataSource.initialize()
+      .then(() => {
+        console.log("Connection established with the MongoDB database");
+      })
+      .catch((error: any) => console.error("Error connecting to MongoDB:", error));
+
+    // Set up routes dynamically using the route configuration
+    routes.map((route) => {
+      app.use(route.path, route.router);
+    });
+
+    // Start the server
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error: any) => console.error("Error connecting to SQL:", error));
