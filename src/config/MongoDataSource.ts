5d4afbd11d2ef405d91c8f16bd5876659e492e3c
communi <expocommuni@gmail.com>
2024-10-31 14:23:31 -0600

    test3

--- src/config/MongoDataSource.ts
+++ src/config/MongoDataSource.ts
@@ -1,18 +1,17 @@
 import * as dotenv from "dotenv";
 import { DataSource } from "typeorm";
-import { Event } from "../entity/MongoDB/Event";
-import { News } from "../entity/MongoDB/news";
 
 dotenv.config();
 
 const mongoUrl = process.env.mongoDB;
+console.log(mongoUrl)
 
 const MongoDataSource = new DataSource({
   type: "mongodb",
   url: mongoUrl,
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  entities: [__dirname + '/entity/**/*.js'],
+  entities: ["dist/entity/MongoDB/*.js"],
   synchronize: true,
   logging: false,
 });
